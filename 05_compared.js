7ce5444730467c7f71afd2ce24e1e7ba571e5a9d
ASHUTOSH RAJ <ashutoshrazz100@gmail.com>
2024-07-23 23:53:13 +0530

    add comparison in code base

---
+++ 05_compared.js
@@ -0,0 +1,35 @@
+// console.log(1 > 2)
+// console.log(1 < 2)
+// console.log(1 >= 2)
+// console.log(1 <= 2)
+// console.log(1 == 2)
+
+
+// console.log("2" > 1) // true
+// console.log("2" < 1) // false
+// console.log("2" == 1) // false
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+
+console.log(undefined == 0) // false
+console.log(undefined > 0) // false
+console.log(undefined < 0) // false
+console.log(undefined >= 0) // false
+console.log(undefined <= 0) // false
+
+
+// === (strict equality)
+
+console.log(1 === 1);
+// Expected output: true
+
+console.log('hello' === 'hello');
+// Expected output: true
+
+console.log('1' === 1);
+// Expected output: false
+
+console.log(0 === false);
+// Expected output: false
