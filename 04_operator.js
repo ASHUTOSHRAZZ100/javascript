bcdbbab97116ea292d7cb746b57ce3abdc249913
ASHUTOSH RAJ <ashutoshrazz100@gmail.com>
2024-07-23 21:43:24 +0530

    add operator in code base

---
+++ 04_operator.js
@@ -0,0 +1,38 @@
+let value = 3
+let negValue = -value
+
+console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2)
+// console.log(2**3)// 2 power 3 or 2^3
+
+let str1 = "Ashutosh"
+let str2 = " Raj"
+let str3 = str1+ str2;
+
+// console.log(str3)
+
+// console.log("1" + 2) // 12
+// console.log(1 + "2") // 12
+// console.log("1" + 2 + 2) //122
+// console.log(1 + 2 + "2") // 32
+
+console.log(+true) // 1
+// console.log(true+) // error
+console.log(true+1) // 2
+console.log(+"") //0
+
+let num1,num2,num3;
+
+num1 = num2 = num3 = 2+2;
+console.table([num1,num2,num3])
+
+let counter = 100
+counter++;
+console.log(counter)
+++counter;
+console.log(counter)
