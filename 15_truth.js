938ef35117f7d41e8c743def671e34bafa19ba21
ASHUTOSH RAJ <ashutoshrazz100@gmail.com>
2024-07-26 22:25:06 +0530

    add Nullish Coalescing Operator (??): null undefined in code base

--- 15_truth.js
+++ 15_truth.js
@@ -46,5 +46,21 @@ else{
 // 0 == ""      true
 
 
+// Nullish Coalescing Operator (??): null undefined
+
+let val1;
+val1 = 5 ?? 10
+
+console.log(val1) // 5
+
+let val2 = null ?? 10
+console.log(val2) // 10
+
+let val3 = undefined ?? 20
+console.log(val3) // 20
+
+let val4 = null ?? 10 ?? 15
+console.log(val4)
+
 
 
