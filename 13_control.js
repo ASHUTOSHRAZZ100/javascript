d2089608bd7431bc1fbc48b876e95a05456b64c4
ASHUTOSH RAJ <ashutoshrazz100@gmail.com>
2024-07-26 20:40:53 +0530

    add control if in code base

---
+++ 13_control.js
@@ -0,0 +1,35 @@
+// if 
+
+// syntax
+
+// if(condition){
+
+// }
+
+// <, >, <=, >=, == (this operator will check only value), !=, === (this operator will check value and Data type), !==
+
+if(2 == "2"){
+    console.log("executed")
+}
+
+if(2 === "2"){
+    console.log("===")
+}
+
+if(2 !== "2"){
+    console.log("!==")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
