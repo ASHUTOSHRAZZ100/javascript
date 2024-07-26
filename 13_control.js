09c6a795d4c943c74aa73c2783352448f57f357f
ASHUTOSH RAJ <ashutoshrazz100@gmail.com>
2024-07-26 20:57:37 +0530

    add else-if in code base

--- 13_control.js
+++ 13_control.js
@@ -53,8 +53,20 @@ console.log("test2"),
 console.log("test3");
 
 
+// else-if
 
-
+if(balance < 500){
+    console.log("less than 500")
+}
+else if(balance < 750){
+    console.log("less than 750")
+}
+else if(balance < 900){
+    console.log("less than 900")
+}
+else{
+    console.log("less than 1200")
+}
 
 
 
