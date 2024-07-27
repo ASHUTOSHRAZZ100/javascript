351a2caf2999df322045676c2830990117d9f498
ASHUTOSH RAJ <ashutoshrazz100@gmail.com>
2024-07-27 20:29:07 +0530

    add foreach loop in code base

--- 16_loop.js
+++ 16_loop.js
@@ -1,10 +1,10 @@
 // for loops
 
-const arr = [1,2,3,4,5]
+const arr = [1, 2, 3, 4, 5]
 
 for (let index = 0; index < arr.length; index++) {
     const element = arr[index];
-    console.log(element)
+    // console.log(element)
 }
 
 // while 
@@ -13,7 +13,7 @@ console.log("while loop")
 let index = 1;
 
 while (index <= 10) {
-    console.log(index)
+    // console.log(index)
     index++;
 }
 
@@ -23,8 +23,8 @@ console.log("Do while loop")
 
 let num1 = 12
 do {
-    console.log(num1)
-    
+    // console.log(num1)
+
 } while (num1 != 12);
 
 // break 
@@ -32,11 +32,11 @@ do {
 index = 1
 
 while (index <= 10) {
-    if(index === 6){
+    if (index === 6) {
         console.log("Break");
         break;
     }
-    console.log(index)
+    // console.log(index)
     index++;
 }
 
@@ -45,12 +45,77 @@ while (index <= 10) {
 index = 1
 
 while (index <= 10) {
-    if(index === 6){
+    if (index === 6) {
         console.log("continue");
         index++;
         continue;
     }
-    console.log(index)
+    // console.log(index)
     index++;
 }
+
+
+
+// foreach Loop
+
+// syntax foreach((item,index,array)=>{})
+
+// Note :- callback function does not have a name 
+
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+coding.forEach(function (item) {
+    console.log(item)
+})
+
+// Or
+
+coding.forEach((val) => {
+    console.log(val)
+})
+
+function printMe(item) {
+    console.log(item)
+}
+
+coding.forEach(printMe)
+
+coding.forEach((item, index, arr) => {
+    console.log(item, index, arr)
+})
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "Python",
+        languageFileName: "py"
+    },
+    {
+        languageName: "C++",
+        languageFileName: "cpp"
+    },
+    {
+        languageName: "Java",
+        languageFileName: "java"
+    },
+]
+
+
+myCoding.forEach((item)=>{
+    console.log(item.languageName)
+})
+
+
+const values = coding.forEach((item)=>{
+    console.log(item)
+    return item
+})
+
+console.log(values)
+
+
+
 
