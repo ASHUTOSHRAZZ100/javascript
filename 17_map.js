9124d12d3a791d1e7b23931fe96e698801ea25fc
ASHUTOSH RAJ <ashutoshrazz100@gmail.com>
2024-07-28 10:15:03 +0530

    add reduce methode in code base

--- 17_map.js
+++ 17_map.js
@@ -26,3 +26,52 @@ newNum = myNumber
 console.log(newNum)
 
 
+
+// reduce method
+
+const array = [1,2,3,4]
+
+const total = array.reduce((acc , currVal)=>{
+    console.log(`acc = ${acc} and curr = ${currVal}`)
+    return acc+currVal
+},0)
+
+console.log(total)
+
+const total1 = array.reduce((acc , currVal)=>{
+    console.log(`acc = ${acc} and curr = ${currVal}`)
+    return acc+currVal
+},2)
+
+console.log(total1)
+
+const total2 = array.reduce((acc , currVal) =>
+  (  console.log(`acc = ${acc} and curr = ${currVal}`), acc+currVal),3)
+
+console.log(total2)
+
+const shoppingCart = [
+    {
+        itemName : "JS course",
+        price : 963,
+    },
+    {
+        itemName : "PY course",
+        price : 900,
+    },
+    {
+        itemName : "RU course",
+        price : 1200,
+    },
+    {
+        itemName : "C++ course",
+        price : 500,
+    },
+
+]
+
+const totalPay = shoppingCart.reduce((acc,item)=>{
+    return acc + item.price
+},0)
+
+console.log(totalPay)
