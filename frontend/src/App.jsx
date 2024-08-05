fa688866320eb0eb73dba4bcc6f8c6dddf64286e
ASHUTOSH RAJ <ashutoshrazz100@gmail.com>
2024-08-05 11:45:16 +0530

    add basic file structure of forntend in code base

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios'
+
+function App() {
+
+  
+
+  return (
+    <>
+      <h1>Chain aur code</h1>
+     
+    </>
+  )
+}
+
+export default App
