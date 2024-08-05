56f2cf80d2fdf2e4ec3330caeefd5e1c247eca2c
ASHUTOSH RAJ <ashutoshrazz100@gmail.com>
2024-08-05 11:47:52 +0530

    add api jokes from backend to frontend in code base

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,32 @@ import axios from 'axios'
 
 function App() {
 
+  const [jokes, setJokes] = useState([])
+  useEffect(() => {
+    axios.get('/api/jokes')
+      .then((response) => {
+        setJokes(response.data)
+      })
+      .catch((error) => {
+        console.log("Error is ", error)
+      })
+  })
   
 
   return (
     <>
       <h1>Chain aur code</h1>
-     
+      <p>JOKES : {jokes.length}</p>
+      {
+        jokes.map((joke, index) => {
+          // console.log(joke);
+          return (<div key={joke.id}>
+            <h3><b>Jokes</b>  {joke.type}</h3>
+            <p><b>Jokes : </b>  {joke.setup}</p>
+            <p> <b>Jokes punchline :</b>  {joke.punchline}</p>
+          </div>)
+        })
+      }
     </>
   )
 }
